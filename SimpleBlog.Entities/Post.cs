26048bc06a138be31da85cab597b6fc87be44b6a
koziuberda <koziuberdaseverin@gmail.com>
2022-01-30 22:18:20 +0200

    Add post title

--- SimpleBlog.Entities/Post.cs
+++ SimpleBlog.Entities/Post.cs
@@ -7,6 +7,7 @@ namespace SimpleBlog.Entities
     {
         public int Id { get; set; }
         public DateTime CreationDate { get; set; }
+        public string Title { get; set; }
         public string Text { get; set; }
         public List<Comment> Comments { get; set; }
     }
