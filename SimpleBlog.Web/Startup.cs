91963d343d6d44bbae80f9681994e3c378d8cd92
koziuberda <koziuberdaseverin@gmail.com>
2022-01-30 19:23:15 +0200

    Add db context as a service

--- SimpleBlog.Web/Startup.cs
+++ SimpleBlog.Web/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SimpleBlog.Data;
 using SimpleBlog.Data.DependencyInjection;
 
 namespace SimpleBlog.Web
@@ -24,9 +26,11 @@ namespace SimpleBlog.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddDbContext<ApplicationContext>(
+                options => options.UseSqlite("Data source=../SimpleBlog.Data/Blog.db"));
             // Use extension
             services.AddRepositoryAndUnitOfWork(); 
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
